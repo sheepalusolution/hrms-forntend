02be75ebf7c89b75d410a19b4c22b31a9239d907
Rijen Maharjan <luckymaharjan044@gmail.com>
2026-02-03 11:11:15 +0545

    windowScrollto

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,91 +1,32 @@
-import React, { useState } from 'react';
-import Footer from '../component/Footer';
+import React, { useState, useEffect } from 'react';
 import Header from '../component/Header';
 import Sidebar from '../component/Sidebar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const UserLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
-      
-      {/* Header */}
       <Header onMenuClick={() => setIsSidebarOpen(true)} />
 
-      {/* Body */}
       <div className="flex flex-1">
-        
-        {/* Sidebar */}
         <Sidebar
           isMenuOpen={isSidebarOpen}
           setIsMenuOpen={setIsSidebarOpen}
         />
 
-        {/* Page Content */}
-        <main
-          className="
-            flex-1 bg-gray-50 overflow-auto
-            md:ml-75
-          "
-        >
+        <main className="flex-1 bg-gray-50 md:ml-75">
           <Outlet />
         </main>
       </div>
-
-      {/* Footer (optional) */}
-      {/* <Footer /> */}
     </div>
   );
 };
 
 export default UserLayout;
-
-
-// import React, { useState } from "react";
-// import Header from "../component/Header";
-// import Sidebar from "../component/Sidebar";
-// import { Outlet, Navigate } from "react-router-dom";
-// import Cookies from "js-cookie";
-
-// const UserLayout = () => {
-//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-//   const role = Cookies.get("role");
-
-//   // 🔐 Block layout if not logged in
-//   if (!role) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return (
-//     <div className="flex flex-col min-h-screen">
-      
-//       {/* Header */}
-//       <Header onMenuClick={() => setIsSidebarOpen(true)} />
-
-//       {/* Body */}
-//       <div className="flex flex-1">
-        
-//         {/* Sidebar */}
-//         <Sidebar
-//           isMenuOpen={isSidebarOpen}
-//           setIsMenuOpen={setIsSidebarOpen}
-//           role={role} // 👈 pass role
-//         />
-
-//         {/* Page Content */}
-//         <main
-//           className="
-//             flex-1 bg-gray-50 overflow-auto
-//             md:ml-75
-//           "
-//         >
-//           <Outlet />
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserLayout;
